d1cc7d8445318702aa8223c145c159022f5f1471
martin_carrizo <carrizofm2010@gmail.com>
2016-06-01 23:40:26 -0300

    primer commit del proyecto

---
+++ Proyect/Banner Ikaro/Assets/script/buttonAbout.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buttonAbout : MonoBehaviour {
+
+	public GameObject imagen;
+	// Use this for initialization
+	private static bool estado=false;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void mostrarImagen(){
+		estado = !estado;
+		imagen.SetActive (estado);
+	}
+}
